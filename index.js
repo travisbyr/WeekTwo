7977ef934feea60045dc12c0226d9c2eb154107e
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-03-02 15:35:04 +1300

    Upload of weekTwo

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log(" ");
+//console .log ("Hello World");
+
+var firstName;
+
+firstName = "Travis"
+//console.log (firstName);
+
+var lastName;
+lastName = "Byrman"
+
+//console.log (lastName);
+
+//console.log (firstName +"\n"+ lastName);
+
+//console.log (firstName +" "+ lastName);
+
+firstNumber = 5
+secondNumber = 6
+
+sum = (firstNumber + secondNumber);
+div = (secondNumber / firstNumber);
+sub = (secondNumber - firstNumber);
+
+console.log ("Sum =", sum);
+console.log ("Sub =", sub);
+console.log ("Div =", div);
